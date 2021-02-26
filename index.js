c3435d355c4cda4b746814766bccd6b3125c185c
SphericalKat <amolele@gmail.com>
2021-02-26 12:49:45 +0530

    feat(treewide): add navigation and set up ios builds
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 /**
  * @format
  */
-
-import {AppRegistry} from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { name as appName } from './app.json';
+import { NavigationContainer } from '@react-navigation/native';
 
-AppRegistry.registerComponent(appName, () => App);
+export default function Main() {
+  return (
+    <NavigationContainer>
+      <PaperProvider>
+        <App />
+      </PaperProvider>
+    </NavigationContainer>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
